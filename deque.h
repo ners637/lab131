156058645a7ce72248f8396d6679f2fe769640c9
mnats <mnacik08@gmail.com>
2026-01-09 14:34:42 +0300

    Добавление структуры дек

---
+++ deque.h
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+typedef struct node {
+    int value;
+    struct node *next;
+    struct node *prev;
+} node_t;
+
+typedef struct deque {
+    node_t *front;
+    node_t *back;
+} deque_t;
+
+void deque_init(deque_t *deque);
+
+int deque_push_front(deque_t *deque, int value);
+int deque_push_back(deque_t *deque, int value);
+
+int deque_pop_front(deque_t *deque, int *value);
+int deque_pop_back(deque_t *deque, int *value);
+
+int deque_add_by_index(deque_t *deque, int index, int value);
+
+void free_deque(deque_t *deque);
+
+void write_to(deque_t* list, char* fname);
+void read_from(char* fname, deque_t* list);
